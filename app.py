353e5ca59177f02c5e3d9052c1e6f58c5f19d0ca
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2021-12-22 01:43:14 +0900

    practice

--- app.py
+++ app.py
@@ -7,22 +7,24 @@ from eda_app import run_eda_app
 from ml_app import run_ml_app
 
 def main():
-    st.title('미국 집값 예측하기')
+    st.title('1990년도 캘리포니아 집값 분석')
     
     menu = ['Home','EDA','ML']
     choice=st.sidebar.selectbox('메뉴',menu)
     
     if choice == 'Home':
-        st.text('미국 집 값을 예측합니다. 조건을 입력하시면 예측이 됩니다')
+        st.text('1990년도 특정 캘리포니아 지역에서 발견된 주택과 관련되어서')
+        st.text('1990년도 데이터를 기반으로 한 자료입니다.')
+        
         st.text('옆에 메뉴를 눌러주세요')
         
-        st.image('http://image.chosun.com/sitedata/image/201810/29/2018102902845_0.jpg')
+        st.image('https://www.visitcalifornia.com/sites/visitcalifornia.com/files/styles/welcome_image/public/vc_perfectbeachtowns_lagunabeach_st_rf_637081844_1280x640_0.jpg')
     
     elif choice == 'EDA':
         run_eda_app()
     
     elif choice == 'ML':
-        run_ml_app()
+        pass
 
 if __name__ == '__main__':
     main()
--- eda_app.py
+++ eda_app.py
@@ -5,49 +5,90 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def run_eda_app():
-    
     st.subheader('EDA 화면 입니다.')
     
-    df = pd.read_csv('data/data.csv')
-    st.dataframe(df)
-    if st.button('통계치'):
+    df=pd.read_csv('data/housing.csv')
+    df.dropna(inplace=True)
+    menu = ['컬럼 정보','데이터 프레임']
+    sel_menu = st.radio('선택하세요',menu)
+    if sel_menu == '컬럼 정보':
+        st.text('longitude = 경도')
+        st.text('latitude = 위도')
+        st.text('housing_median_age = 주택 지어진 기간')
+        st.text('total_rooms = 블록 내 총 방수')
+        st.text('total_bedrooms = 한 블록 내 침실 수')
+        st.text('population = 인구수')
+        st.text('households = 세대수')
+        st.text('median_income = 중간 소득')
+        st.text('median_house_value = 중간 주택 가격')
+        st.text('ocean_proximity = 바다 근접도')
+    else :
+        st.dataframe(df)
         st.write(df.describe())
-        
-    selected_cols=st.multiselect('보고싶은 컬럼을 선택하세요',df.iloc[:,1:].columns)
-    if len(selected_cols) > 0 :
-        st.write(df[selected_cols])
-    else :
-        st.write('선택된 컬럼이 없습니다')
-        
-    st.subheader('선택한 컬럼들의 상관관계 분석')
-    st.text('데이터 전체의 히트맵')
-    fig1=plt.figure(figsize=(10,5))
-    sns.heatmap(df.corr(),annot=True,fmt='.1f',cmap='RdPu')
-    plt.title('data correlation')
-    st.pyplot(fig1)
-    st.write(df.corr())
+
+    st.subheader('전체 데이터 시각화')
+    st.map(df)
+    df.hist(figsize=(10,10),bins=10)
+    st.pyplot()
     
-    selected_cols_corr=st.multiselect('컬럼들을 선택해주세요.',df.drop(columns=['date','street','city','statezip','country'],axis=1).columns)
-    if len(selected_cols_corr) != 0 :
-        st.write(df[selected_cols_corr].corr())
-        fig2=sns.pairplot(data=df[selected_cols_corr],kind='reg')
-        st.pyplot(fig2)
+    st.subheader('원하는 컬럼 보기')
+    sel_col = st.multiselect('보고 싶은 컬럼을 선택 해주세요',df.columns)
+    if len(sel_col) != 0:
+        st.write(df[sel_col])
     else :
-        st.write('선택한 컬럼이 없습니다.')
+        st.write('선택한 컬럼이 없습니다')
         
-    st.subheader('집값이 가장 높은 곳과 가장 낮은 곳')
-    df_ch=df.loc[df['price'] != 0,]
+    st.subheader('인구수가 가장 높은 곳과 낮은 곳')
     radio_menu = ['높은곳','낮은곳']
     choice_radio=st.radio('선택하세요',radio_menu)
     if choice_radio == '높은곳':
-        st.dataframe(df_ch.loc[df_ch['price'].max()==df_ch['price'],])
+        st.dataframe(df.loc[df['population'].max() == df['population'],])
     else :
-        st.dataframe(df_ch.loc[df_ch['price'].min()==df_ch['price'],])
+        st.dataframe(df.loc[df['population'].min() == df['population'],])
         
-    st.subheader('각 도시에 따른 평균 가격')
-    city = df['city'].unique()
-    select_city=st.selectbox('도시를 선택해주세요',city)
+    st.subheader('가격에 따른 주택 데이터 표시')
+    number = st.slider('선택',value=[df['median_house_value'].min(),df['median_house_value'].max()],min_value=df['median_house_value'].min(),max_value=df['median_house_value'].max())
+    st.write(df.loc[(df['median_house_value']>=number[0])&(df['median_house_value']<=number[1]),])
     
-    st.write(df.groupby('city').mean().loc[:,:'price'].loc[select_city])
+        
+    st.subheader('데이터 시각화')
+    menu1=['위도와 경도에 따른 분포','건축 년도에 따른 주택 가격 평균','바다 근접도에 따른 가격 평균','상관관계 분석']
+    sel_chart=st.selectbox('선택',menu1)
+    if sel_chart == '위도와 경도에 따른 분포':
+        df.plot(x='longitude',y='latitude',kind='scatter')
+        plt.title('Distribution by latitude and longitude')
+        st.pyplot()
+    elif sel_chart == '건축 년도에 따른 주택 가격 평균':
+        period = df.groupby('housing_median_age')['median_house_value'].mean()
+        period.sort_values(inplace = True)
+        index = period.index
+        fig1 = plt.figure()
+        plt.bar(index,period)
+        plt.title('Housing_median_age Average Price')
+        plt.xlabel('Housing_median_age')
+        plt.ylabel('Average Price')
+        st.pyplot(fig1)
+        st.write(period)
+    elif sel_chart == '바다 근접도에 따른 가격 평균':
+        price = df.groupby('ocean_proximity')['median_house_value'].mean()
+        price.sort_values(inplace=True)
+        lable=price.index
+        fig2 = plt.figure()
+        plt.bar(lable,price)
+        plt.title('Ocean_proximity Average Price')
+        plt.ylabel('Average Price')
+        st.pyplot(fig2)
+        st.write(price)
+    elif sel_chart == '상관관계 분석':
+        fig3=plt.figure(figsize=(10,8))
+        sns.heatmap(df.drop(columns=['longitude','latitude'],axis=1).corr(), annot=True, cmap='RdPu')
+        plt.title('data correlation')
+        st.pyplot(fig3)
+        st.write(df.drop(columns=['longitude','latitude'],axis=1).corr())
+        
+    
+        
+    
+  
         
     
